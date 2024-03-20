3259f07b7faea0a4c7b0614b0bed41155c6b566b
Muhammad Usman <163582723+musman023@users.noreply.github.com>
2024-03-20 23:32:57 +0500

    lists ui

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
@@ -23,10 +27,10 @@ class _ProductListScreenState extends State<ProductListScreen> {
     'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
     'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg',
     'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
-    'https://www.shutterstock.com/image-photo/bunch-bananas-isolated-on-white-background-99478112',
-    'https://www.shutterstock.com/image-photo/cherry-trio-stem-leaf-57701209',
-    'https://www.shutterstock.com/image-photo/peach-isolated-on-white-345189368',
-    'https://www.istockphoto.com/photo/fruit-background-gm529664572-93294653?searchscope=image%2Cfilm'
+    'https://img.freepik.com/free-photo/ai-generated-image-banana_23-2150683014.jpg?t=st=1710958290~exp=1710961890~hmac=1540be32872c5b203ed9ce2b1fb041aa54878ed1f0eb918e62696cfdeed9a7f9&w=360',
+    'https://img.freepik.com/free-vector/realistic-berries-composition-with-isolated-image-cherry-with-ripe-leaves-blank-background-vector-illustration_1284-66040.jpg?t=st=1710958352~exp=1710961952~hmac=3fd2aee8391650ef06f8dc745a8b1826eca29c523a41500873165836e87f1b83&w=740',
+    'https://img.freepik.com/free-vector/realistic-fruits-composition-with-images-whole-sliced-apricot-fruit-blank-background-vector-illustration_1284-66028.jpg?t=st=1710958418~exp=1710962018~hmac=a2be1250879c47fc01b2e7906b88e74c38e653d1c6d248bb2dfe20c113f94bc4&w=740',
+    'https://img.freepik.com/free-photo/vibrant-collection-healthy-fruit-vegetables-generated-by-ai_24640-80425.jpg?t=st=1710958578~exp=1710962178~hmac=8de652a8820d76f74cd3d232bba9858ef89fc9124adf213fc3d1fd81368e191b&w=996'
   ];
   @override
   Widget build(BuildContext context) {
@@ -42,6 +46,85 @@ class _ProductListScreenState extends State<ProductListScreen> {
           SizedBox(
             width: 20,
           ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: productName.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Image(
+                                    fit: BoxFit.cover,
+                                    height: 100,
+                                    width: 100,
+                                    image: NetworkImage(
+                                      productImage[index].toString(),
+                                    )),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        productName[index].toString(),
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(productUnit[index].toString() +
+                                          ' ' +
+                                          r"$" +
+                                          productPrice[index].toString()),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Container(
+                                          height: 35,
+                                          width: 100,
+                                          decoration: BoxDecoration(
+                                            color: Colors.green,
+                                            borderRadius:
+                                                BorderRadius.circular(5),
+                                          ),
+                                          child: const Center(
+                                            child: Text(
+                                              "Add to Cart",
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }))
         ],
       ),
     );
